9ec9eb05d0cb60bf757368df71f5ba7246672273
SilasReinagel <silas.reinagel@gmail.com>
2020-04-11 17:40:48 -0700

    Added Tooltip Feature. Closes #17

---
+++ src/ModeaJet/Assets/Scripts/UI/HoverTooltip.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public sealed class HoverTooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private string tooltip;
+    
+    public void OnPointerEnter(PointerEventData eventData) => Message.Publish(new ShowUiTooltip { Tooltip = tooltip });
+    public void OnPointerExit(PointerEventData eventData) => Message.Publish(new ShowUiTooltip { Tooltip = "" });
+}
---
+++ src/ModeaJet/Assets/Scripts/UI/ShowUiTooltip.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+[Serializable]
+public class ShowUiTooltip
+{
+    public string Tooltip;
+}
