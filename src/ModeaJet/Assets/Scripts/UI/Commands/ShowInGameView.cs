1c3c44ac6c1b8f25788adc3db0637a2b499651cb
SilasReinagel <silas.reinagel@gmail.com>
2020-04-11 22:31:58 -0700

    Modal In-Game Views. Closes #24

---
+++ src/ModeaJet/Assets/Scripts/UI/Commands/ShowInGameView.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+[Serializable]
+public class ShowInGameView
+{
+    public InGameViewId View;
+}
---
+++ src/ModeaJet/Assets/Scripts/UI/InGameViewManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public sealed class InGameViewManager : OnMessage<ShowInGameView, DismissCurrentView>
+{
+    [SerializeField] private GameObject hudView;
+    [SerializeField] private GameObject dialogueView;
+    [SerializeField] private GameObject dilemmaView;
+
+    private GameObject _currentView;
+    private InGameViewId _currentId;
+
+    private void Awake() => ShowHudView();
+
+    protected override void Execute(ShowInGameView msg)
+    {
+        if (msg.View == InGameViewId.Dilemma)
+            ShowView(InGameViewId.Dilemma, dilemmaView);
+        else if (msg.View == InGameViewId.Dialogue)
+            ShowView(InGameViewId.Dialogue, dialogueView);
+        else if (msg.View == InGameViewId.Hud)
+            ShowHudView();
+        else
+            Debug.LogError($"No View for {msg.View} setup in {nameof(InGameViewManager)}");
+    }
+
+    protected override void Execute(DismissCurrentView msg) => Dismiss();
+    public void Dismiss() => ShowHudView();
+
+    private void ShowHudView() => ShowView(InGameViewId.Hud, hudView);
+
+    private void ShowView(InGameViewId id, GameObject view)
+    {
+        if (_currentId == id)
+            return;
+        
+        if (_currentView != null)
+            _currentView.SetActive(false);
+        _currentId = id;
+        _currentView = view;
+        _currentView.SetActive(true);
+    }
+}
