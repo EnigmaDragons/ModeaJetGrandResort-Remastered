4ace595cfd3659586d8c48c64f89e0e6b57dd1ff
noah reinagel <noah.r9@gmail.com>
2020-04-15 22:52:37 -0700

    implemented GoToNodeTree handling

---
+++ src/ModeaJet/Assets/NodeSystem/Scripts/UI/GoToNodeTreeHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace EnigmaDragons.NodeSystem
+{
+    public class GoToNodeTreeHandler : OnMessage<GoToNodeTree>
+    {
+        [SerializeField] private CurrentNodeTree currentNodeTree;
+        protected override void Execute(GoToNodeTree msg) => currentNodeTree.StartNodeTree(msg.NodeTree);
+    }
+}
