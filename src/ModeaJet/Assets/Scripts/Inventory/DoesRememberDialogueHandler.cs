1aa10c4781af29fd23072149672655381b0d3e18
noah reinagel <noah.r9@gmail.com>
2020-04-15 22:28:35 -0700

    added inverse of conditions

--- src/ModeaJet/Assets/Scripts/Inventory/DoesRememberDialogueHandler.cs
+++ src/ModeaJet/Assets/Scripts/Inventory/DoesRememberDialogueHandler.cs
@@ -7,5 +7,9 @@ public class DoesRememberDialogueHandler : NodeConditionHandler
 {
     [SerializeField] private CurrentGameState gameState;
     public override Type CondtionType => typeof(DoesRememberDialogue);
-    public override bool IsConditionMet(object condition) => gameState.GameState.HasViewedItem(((DoesRememberDialogue)condition).Dialogue);
+    public override bool IsConditionMet(object condition)
+    {
+        var doesRemember = (DoesRememberDialogue) condition;
+        return gameState.GameState.HasViewedItem(doesRemember.Dialogue) != doesRemember.Inverse;
+    }
 }
